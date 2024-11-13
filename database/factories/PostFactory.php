cad8e93a38133349ae8e214963c006999ed17358
DESKTOP-7VUTVPT\Emilio <1448592@alu.murciaeduca.es>
2024-11-13 11:26:46 +0100

    no message

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,9 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 
-/**
- * ffff
- */
 class PostFactory extends Factory
 {
     /**
